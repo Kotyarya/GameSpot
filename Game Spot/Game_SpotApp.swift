dccaf9591d1cb17b9ad9873aa83cfa2b405cf727
Kotyarya <sunguuner500@gmail.com>
2026-04-27 15:11:23 +0200

    Initial Commit

---
+++ Game Spot/Game_SpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_SpotApp.swift
+//  Game Spot
+//
+//  Created by Maksim Aksamitnuy on 27/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Game_SpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
